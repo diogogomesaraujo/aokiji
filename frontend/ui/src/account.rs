a0195269e0eecbd3dae2794f77153902506c33d5
diogogomesaraujo <118282799+diogogomesaraujo@users.noreply.github.com>
2025-03-06 10:50:41 +0000

    adding pending and recievable

--- frontend/ui/src/account.rs
+++ frontend/ui/src/account.rs
@@ -13,9 +13,9 @@ pub fn Balance() -> Element {
         document::Link { rel: "stylesheet", href: ACCOUNT_CSS }
         div {
             id: "card",
-            span { id: "secondary" , style: "display: inline-block; margin-bottom: 20px;", "TOTAL BALANCE" }
+            span { id: "secondary" , style: "display: inline-block; margin-bottom: 14px;", "TOTAL BALANCE" }
             div {
-                id: "container",
+                style: "display: inline-block; margin-bottom: 36px;",
                 div {
                     id: "fill-card",
                     span { id: "sub-heading" , "XNO" }
@@ -27,6 +27,24 @@ pub fn Balance() -> Element {
                     div {
                         id: "secondary" ,
                         strong { id: "sub-heading" , "0.0005€" }
+                    }
+                }
+            }
+            div {
+                div {
+                    id: "container",
+                    style: "display: inline-block; margin-bottom: 14px;",
+                    div {
+                        id: "fill-card",
+                        span { id: "secondary", "PENDING" }
+                    }
+                }
+                div {
+                    id: "container",
+                    div {
+                        id: "fill-card",
+                        span { style: "display: inline-block; padding-right: 10px; align-items: center;", "XNO" }
+                        strong { id: "sub-heading" , "0.0005" }
                     }
                 }
             }
